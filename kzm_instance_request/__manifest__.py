304d455019791cf271c3b6d878cd8fda956b1749
ZAKARIAELGHANDORI <z.elghandori@karizma.ma>
2022-12-29 17:33:48 +0100

    [UPD]:Modification des droit d'accées selon le group existant

--- kzm_instance_request/__manifest__.py
+++ kzm_instance_request/__manifest__.py
@@ -24,6 +24,7 @@
     # always loaded
     'data': [
         'security/ir.model.access.csv',
+        'security/security.xml',
         'views/odoo_version.xml',
         'views/instances.xml',
         'data/instancedata.xml',
